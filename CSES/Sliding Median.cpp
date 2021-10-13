1803e9ca9a4a79e927e8a32d672536f610187750
wuda20 <71037659+wuda20@users.noreply.github.com>
2021-10-13 18:08:59 -0400

    Update Sliding Median.cpp

--- CSES/Sliding Median.cpp
+++ CSES/Sliding Median.cpp
@@ -1,1 +1,16 @@
-
+multiset<int> lo, hi;
+int n,k;
+vector<int> v(n);
+int main() {
+  cin >> n >> k;
+  for (int i=0;i<n;++i) cin >> v[i];
+  for (int i=0;i<k;++i) {
+    lo.insert(nums[i]);
+    if (i>k/2) {
+      hi.insert(lo.rbegin());
+      lo.erase(lo.rbegin());
+    }
+  }
+  for (int i=k;i<n;++i){
+    lo.insert(nums[i]));
+}
